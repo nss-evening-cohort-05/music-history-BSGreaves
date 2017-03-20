a83f52f9beac7dabe8eed6252c2550cce38551e8
Ben Greaves <bsgreaves@gmail.com>
2017-03-20 15:52:49 -0500

    About 30% done

--- view.js
+++ view.js
@@ -1,59 +1,80 @@
-
 var songs = [];
 var outputDiv = document.getElementById("songdisplaydiv");
 
-songs[songs.length] = "Sofi Needs a Ladder - by Deadmau5 on the album 4x4=12";
-songs[songs.length] = "Another Brick in the Wall - by Pink Floyd on the album The Wall";
-songs[songs.length] = "The Package - by A Pefect Circle on the album Thirteenth Step";
-
-function printToArray (array) {
-	outputDiv.innerHTML = "";
-	for (let i = 0; i < array.length; i++) {
-		outputDiv.innerHTML += "<article class='songdiv'><p class='songartistname songproperties'>" + array[i] + "</article></p>";
-	}
-};
-
-printToArray(songs);
-
-// View Section
+// "View" Section
 
 var viewLink = document.getElementById("view-link");
 var viewSection = document.getElementById("view-section");
 
-viewLink.addEventListener("click", function() {
-  viewSection.classList.add("visible");
-  viewSection.classList.remove("hidden");
+viewLink.addEventListener("click", displayView);
 
-  addSection.classList.add("hidden");
-  addSection.classList.remove("visible");
-});
+function displayView() {
+    viewSection.classList.add("visible");
+    viewSection.classList.remove("hidden");
+    addSection.classList.add("hidden");
+    addSection.classList.remove("visible");
+};
 
-
-// Add Section
+// "Add" Section
 
 var addLink = document.getElementById("add-link");
 var addSection = document.getElementById("add-section");
 
-addLink.addEventListener("click", function() {
-  viewSection.classList.add("hidden");
-  viewSection.classList.remove("visible");
+addLink.addEventListener("click", displayAdd);
 
-  addSection.classList.add("visible");
-  addSection.classList.remove("hidden");
-});
+function displayAdd() {
+    viewSection.classList.add("hidden");
+    viewSection.classList.remove("visible");
+    addSection.classList.add("visible");
+    addSection.classList.remove("hidden");
+};
 
 var addSongField = document.getElementById("addSongField");
 var addArtistField = document.getElementById("addArtistField");
-var addAlbumField = document.getElementById("addAlbumField");	
-var addSongButton = document.getElementById("addSongButton");	
+var addAlbumField = document.getElementById("addAlbumField");
+var addSongButton = document.getElementById("addSongButton");
 
 addSongButton.addEventListener("click", addToSongList);
 
-function addToSongList () {
-	let currSong = addSongField.value;
-	let currArtist = addArtistField.value;
-	let currAlbum = addAlbumField.value;
-	let newSong = `${currSong} - by ${currArtist} on album ${currAlbum}`;
-	songs.push(newSong);
-	printToArray(songs);
+function addToSongList() {
+    if (addSongField.value !== "" && addArtistField.value !== "" && addAlbumField.value !== "") {
+        newSong = {}
+        newSong.title = addSongField.value;
+        newSong.artist = addArtistField.value;
+        newSong.album = addAlbumField.value;
+        songs.push(newSong);
+        printToDOM(songs);
+        displayView();
+        addSongField.value = "";
+        addArtistField.value = "";
+        addAlbumField.value = "";
+    }
+};
+
+function printToDOM(array) {
+    outputString = "";
+    for (let i = 0; i < array.length; i++) {
+        outputString += `<article class='songdiv'><p class='songartistname songproperties'>`
+        outputString += `${array[i].title} by ${array[i].artist} on the album ${array[i].album}`
+        outputString += `</article></p>`;
+    }
+    outputDiv.innerHTML = outputString
+};
+
+// On Page Load
+
+var songs = [];
+var outputDiv = document.getElementById("songdisplaydiv");
+
+function parseJson() {
+    var data = JSON.parse(this.responseText);
+    data = data.songs;
+    data.forEach(each => songs.push(each))
+    printToDOM(songs);
 }
+
+var songsJson = new XMLHttpRequest();
+songsJson.addEventListener("load", parseJson);
+songsJson.addEventListener("error", function(){console.log("JSON Error")});
+songsJson.open("GET", "songs.json");
+songsJson.send();
